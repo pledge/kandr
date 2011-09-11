6409a56a17ebece2f0c6bec64fa2e9772b32c9f7
Martin Gilday <mgilday@imap.cc>
2011-09-11 12:00:58 +0100

    Sorting lines from stdin

---
+++ ch04/qsort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void qsort(int[], int, int);
+void swap(int[], int, int);
+
+int main()
+{
+	int values[] = { 5,2,1,4,9,2,3,12 };
+	printf("Unordered:\n");
+	for(int i = 0; i < 8; i++)
+		printf("%i\n", values[i]);
+	qsort(values, 0, 7);
+	printf("Sorted:\n");
+	for(int i = 0; i < 8; i++)
+		printf("%i\n", values[i]);
+}
+
+void qsort(int v[], int left, int right)
+{
+	int i, last;
+	void swap(int v[], int i, int j);
+
+	if (left >= right)
+		return;
+	swap(v, left, (left + right) / 2);
+	last = left;
+	for (i = left + 1; i <= right; i++)
+		if (v[i] < v[left])
+			swap(v, ++last, i);
+	swap(v, left, last);
+	qsort(v, left, last - 1);
+	qsort(v, last + 1, right);
+}
+
+void swap(int v[], int i, int j)
+{
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;	
+}
+
